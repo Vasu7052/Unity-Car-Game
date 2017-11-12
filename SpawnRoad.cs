66effcfa9abc5e21d65db87f77a37b6ca4ad4517
Vasu Gupta <androfly.official@gmail.com>
2017-11-12 19:22:35 +0530

    Extreme Car Rider

--- SpawnRoad.cs
+++ SpawnRoad.cs
@@ -3,9 +3,10 @@ using System.Collections;
 
 public class SpawnRoad : MonoBehaviour {
 
-	public float currentTrackNumber;
+	float currentTrackNumber;
 	public float lengthOfTrack = 30f ;
-	public GameObject road ;
+	public GameObject[] roads ;
+	int randRoad ;
 
 	// Use this for initialization
 	void Start () {
@@ -15,11 +16,25 @@ public class SpawnRoad : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		currentTrackNumber = gameObject.transform.position.z / lengthOfTrack;
-		Debug.Log (System.Math.Round(currentTrackNumber,0));
-		if(gameObject.transform.position.z == ((currentTrackNumber+1)*lengthOfTrack-15)){
-			GameObject spawnRoad = Instantiate (road , new Vector3(0f,6f,((currentTrackNumber+1)*lengthOfTrack)) , gameObject.transform.rotation);
+		currentTrackNumber = Mathf.Ceil (currentTrackNumber);
+		if(gameObject.transform.position.z > ((currentTrackNumber*lengthOfTrack)-25) && gameObject.transform.position.z < ((currentTrackNumber*lengthOfTrack)-24)){
+			randRoad = Random.Range (0, 3);
+			GameObject spawnRoad = Instantiate (roads[randRoad] , new Vector3(0f,6f,((currentTrackNumber)*lengthOfTrack)) , gameObject.transform.rotation) as GameObject;
 			spawnRoad.transform.localScale = new Vector3 (1f,1f,1f);
 		}
 
 	}
+
+	void OnCollisionEnter(Collider col){
+		if(col.gameObject.name == "SpawnBar"){
+			Debug.Log ("Triggered");
+		}
+	}
+
+	void OnTriggerEnter(Collider col){
+			Debug.Log ("Triggered1");
+	}
+
+
+
 }
